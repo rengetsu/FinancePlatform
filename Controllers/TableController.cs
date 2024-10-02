6efe0405fa3cab3eef5558692b798ed65979e35f
Pavel Trostianko <rengetsu@mail.ru>
2024-10-02 21:50:55 +0300

    Table view add

---
+++ Controllers/TableController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FinancePlatform.Controllers
+{
+    public class TableController : Controller
+    {
+        // Action method to serve the Table.cshtml view
+        public IActionResult Table()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
