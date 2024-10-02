52e03b956c396a377f730dc086fd626da1b31fec
Pavel Trostianko <rengetsu@mail.ru>
2024-10-02 21:21:43 +0300

    Create view for Chart

---
+++ Controllers/ChartController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FinancePlatform.Controllers
+{
+    public class ChartController : Controller
+    {
+        // Action method to serve the Chart.cshtml view
+        public IActionResult Chart()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            // Example data, replace with actual data from your database
+            var chartData = new List<int> { 10, 20, 30, 40, 50 };
+
+            ViewBag.ChartData = chartData;
+
+            return View("Chart");
+        }
+    }
+}
